ad1f6edc390e87c4a5cf633867920f286da01a99
M V K <krishna.mugala9@gmail.com>
2022-08-30 13:22:33 +0530

    added getter and setter methods in the class VehicleManufacturer

--- src/main/java/com/vehicle/VehicleManufacturer.java
+++ src/main/java/com/vehicle/VehicleManufacturer.java
@@ -21,4 +21,28 @@ public abstract class VehicleManufacturer {
         this.vehicleModelName = vehicleModelName;
         this.vehicleType = vehicleType;
     }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+
+    public String getVehicleModelName() {
+        return vehicleModelName;
+    }
+
+    public void setVehicleModelName(String vehicleModelName) {
+        this.vehicleModelName = vehicleModelName;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
 }
