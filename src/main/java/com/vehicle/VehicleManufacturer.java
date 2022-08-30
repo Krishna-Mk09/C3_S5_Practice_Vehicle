271393f05ae7e8a990853b942f1a5c80812f81a5
M V K <krishna.mugala9@gmail.com>
2022-08-30 13:17:17 +0530

    created the abstract class in the package.

---
+++ src/main/java/com/vehicle/VehicleManufacturer.java
@@ -0,0 +1,13 @@
+/*
+ * Author Name : M.Krishna.
+ * Date: 30-08-2022
+ * Created With: IntelliJ IDEA Community Edition
+ *
+ */
+
+
+package com.vehicle;
+
+public abstract class VehicleManufacturer {
+
+}
