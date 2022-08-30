f98d15f7fc17b678f2bc95315e54e000ee081261
M V K <krishna.mugala9@gmail.com>
2022-08-30 17:29:23 +0530

    added the string to display in the console

--- src/main/java/com/vehicle/VehicleManufacturerImpl.java
+++ src/main/java/com/vehicle/VehicleManufacturerImpl.java
@@ -12,9 +12,9 @@ public class VehicleManufacturerImpl {
     public static void main(String[] args) {
         Bike bike = new Bike();
         System.out.println(bike.toString());
-        System.out.println(bike.maxSpeed("sportsBike"));
+        System.out.println(" The speed of the Bike is : " + bike.maxSpeed("sportsBike"));
         Car car = new Car();
-        System.out.println(car.maxSpeed("sedan"));
+        System.out.println(" The speed of the car is : JKK" + car.maxSpeed("sedan"));
 
     }
 }
